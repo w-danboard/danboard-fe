a554b00333fb1c58b6f9142cb1dac01e65c23600
wanglin <wanglin46@lenovo.com>
2020-08-27 14:08:46 +0800

    登录页动画

--- src/main.js
+++ src/main.js
@@ -5,14 +5,13 @@ import App from './App.vue'
 import router from './routers'
 
 // 引入样式
-// import '@/assets/styles/main.css' 			        // 公共样式
+import '@/assets/styles/main.css' 			        // 公共样式
 import 'element-ui/lib/theme-chalk/index.css'   // elment-ui 样式
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI, { locale, size: 'small' })
 
-console.log(router)
 new Vue({
   router,
   render: h => h(App),
